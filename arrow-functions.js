f909369a1a98c0d0f7d48f4694f93c650b39439a
tWilltp <williamjenkinsfeb@gmail.com>
2023-03-01 18:02:51 +0000

    arrow functions

--- arrow-functions.js
+++ arrow-functions.js
@@ -5,10 +5,39 @@
 
 // Vanilla JavaScript Function
 
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+let sum = addTwoNumbers(3, 5);
+console.log(sum);
+
 // Arrow Function With Parameters
 
+const addTwoNumbers2 = (a, b) => {
+    return a + b;
+}
+let sum2 = addTwoNumbers(3, 5);
+console.log(sum);
+
 // Single Line Arrow Function With Parameters
+// const addTwoNumbers3 = (a, b) => a + b; also valid syntax
+const addTwoNumbers3 = (a, b) => a + b;
+let sum3 = addTwoNumbers3(3, 5);
+console.log(sum3);
 
 // Implicit Returns
 
+const saySomething = mesage => console.log(message);
+saySomething('Hello there');
+
+const sayHello = () => console.log('General kenobi')
+sayHello();
+
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `p
+    multi string
+    p`
+)
+console.log(returnMultipleLines());
